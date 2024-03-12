6227685b0bba5f3fd426585bda8403e11d8ac25c
Catherine <134451872+ErinRas@users.noreply.github.com>
2024-03-12 15:50:35 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <the-header></the-header>
+  <router-view v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+  <the-footer></the-footer>
+</template>
+
+<script>
+import { mapMutations, mapActions } from 'vuex'
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
+
+export default {
+  components: { TheHeader, TheFooter },
+  created() {
+    const userAccessKey = localStorage.getItem('userAccessKey')
+    if (userAccessKey) {
+      this.updateAccessKey(userAccessKey)
+    }
+    this.loadCart()
+  },
+  methods: {
+    ...mapActions(['loadCart']),
+    ...mapMutations(['updateAccessKey']),
+  },
+}
+</script>
+
+<style>
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.35s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.35s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
---
+++ src/components/BaseFormRadioDelivery.vue
@@ -0,0 +1,22 @@
+<template>
+  <base-form-radio :group="group">
+    <span class="options__value">
+      {{ title }} <b v-if="+price">{{ `${pricePretty} ₽` }}</b> <b v-else>бесплатно</b>
+    </span>
+  </base-form-radio>
+</template>
+
+<script>
+import BaseFormRadio from '@/components/BaseFormRadio.vue'
+import numberFormat from '@/helpers/numberFormat.js'
+
+export default {
+  components: { BaseFormRadio },
+  props: ['title', 'price', 'group'],
+  computed:{
+    pricePretty(){
+return numberFormat(this.price)
+    }
+  }
+}
+</script>
